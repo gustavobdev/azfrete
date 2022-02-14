871889eb8726cc58134b398923badc1a6d1e3c29
Gustavo Bomfim <gustavobdev@gmail.com>
2022-02-14 15:26:10 -0300

    public

---
+++ src/main/java/com/gbs/azfrete/AzfreteApplication.java
@@ -0,0 +1,13 @@
+package com.gbs.azfrete;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AzfreteApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AzfreteApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/gbs/azfrete/AzfreteApplicationTests.java
@@ -0,0 +1,13 @@
+package com.gbs.azfrete;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AzfreteApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
